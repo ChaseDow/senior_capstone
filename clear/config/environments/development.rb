8dbe43aa7291a37738c51ff01a00bdd25cc377af
Chase <chase@Chase.localdomain>
2026-01-09 01:21:24 -0600

    Fixing auto fill bug and errors for mailer

--- clear/config/environments/development.rb
+++ clear/config/environments/development.rb
@@ -16,8 +16,11 @@ Rails.application.configure do
   config.server_timing = true
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
 
 
 
