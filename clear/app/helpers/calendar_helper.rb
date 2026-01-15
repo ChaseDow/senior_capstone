f131f48a7968a65d002d72f490978b3fe09e3999
Chase <chase@Chase.localdomain>
2026-01-15 01:58:08 -0600

    remove unneeded files

--- clear/app/helpers/calendar_helper.rb
+++
@@ -1,8 +0,0 @@
-module CalendarHelper
-  def calendar_weeks(date)
-    start_date = date.beginning_of_month.beginning_of_week(:sunday)
-    end_date   = date.end_of_month.end_of_week(:sunday)
-
-    (start_date..end_date).to_a.in_groups_of(7)
-  end
-end
