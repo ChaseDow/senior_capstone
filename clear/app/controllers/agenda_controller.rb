1ce41ed3eff05fb194708acf144ec418f3fc1455
eulo0 <bobbyjones10102@gmail.com>
2026-01-16 00:01:13 +0000

    WIP: Controller

--- clear/app/controllers/agenda_controller.rb
+++ clear/app/controllers/agenda_controller.rb
@@ -5,31 +5,23 @@ class AgendaController < ApplicationController
   before_action :set_page_title
 
   def index
-    @events = current_user.events.order(starts_at: :asc)
-    @courses = current_user.courses.order(starts_at: :asc)
+  day = Date.today.strftime("%A")
+  day_to_abbreviation = {"Monday"=> "M", "Tuesday" => "T", "Wednesday" => "W", "Thursday" => "R", "Friday" => "F"}
+  
+  @events = current_user.events.where(starts_at: Date.today.all_day)
+  @courses = Course.where("meeting_days LIKE ?", "%#{day_to_abbreviation[day]}%")
+  
+  @agenda = (@events.map { |e| {item: e, type: "Event", time: e.starts_at.strftime("%H:%M")}} +
+             @courses.map { |c| { item: c, type: "Course", time: c.start_time.strftime("%H:%M")}})
+            .sort_by {|entry| entry[:time]}
   end
 
   def show; end
 
-  def new
-    @event = current_user.events.new
-  end
-
   private
 
   def set_page_title
-    @page_title =
-      case action_name
-      when "index" then "Events"
-      else "Event"
-      end
+    @page_title = Agenda
   end
 
-  def set_event
-    @event = current_user.events.find(params[:id])
-  end
-
-  def event_params
-    params.require(:event).permit(:title, :starts_at, :ends_at, :location, :description)
-  end
 end
