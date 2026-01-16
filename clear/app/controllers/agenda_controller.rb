5658a17bc88bcbf584d7b4d0597bd5449d06bfd6
eulo0 <bobbyjones10102@gmail.com>
2026-01-16 02:26:40 +0000

    Updated view for agenda

--- clear/app/controllers/agenda_controller.rb
+++ clear/app/controllers/agenda_controller.rb
@@ -5,11 +5,13 @@ class AgendaController < ApplicationController
   before_action :set_page_title
 
   def index
+  @day = Date.today.strftime("%A")
   day = Date.today.strftime("%A")
   day_to_abbreviation = {"Monday"=> "M", "Tuesday" => "T", "Wednesday" => "W", "Thursday" => "R", "Friday" => "F"}
-  
   @events = current_user.events.where(starts_at: Date.today.all_day)
-  @courses = Course.where("meeting_days LIKE ?", "%#{day_to_abbreviation[day]}%")
+  #@events = current_user.events.order(starts_at: :asc)
+  @courses = Course.where("meeting_days LIKE ?", ["%",day_to_abbreviation[day],"%"].join)
+  #@courses = Course.where("meeting_days LIKE ?", "%R%")
   
   @agenda = (@events.map { |e| {item: e, type: "Event", time: e.starts_at.strftime("%H:%M")}} +
              @courses.map { |c| { item: c, type: "Course", time: c.start_time.strftime("%H:%M")}})
