7b53614e25687cbb9d8eb1c27d564ef97622f317
eulo0 <bobbyjones10102@gmail.com>
2026-01-28 21:11:52 +0000

    added icon, title/subtitle changes, and other minor changes

--- clear/app/controllers/syllabuses_controller.rb
+++ clear/app/controllers/syllabuses_controller.rb
@@ -3,21 +3,17 @@ class SyllabusesController < ApplicationController
   layout "app_shell"
   before_action :set_syllabus, only: [:show, :destroy]
 
-  # GET /syllabuses
   def index
     @syllabuses = Syllabus.all.order(created_at: :desc)
   end
 
-  # GET /syllabuses/1
   def show
   end
 
-  # GET /syllabuses/new
   def new
     @syllabus = Syllabus.new
   end
 
-  # POST /syllabuses
   def create
     @syllabus = Syllabus.new(syllabus_params)
 
@@ -28,18 +24,18 @@ class SyllabusesController < ApplicationController
     end
   end
 
-  # DELETE /syllabuses/1
   def destroy
     @syllabus.destroy
     redirect_to syllabuses_url, notice: 'Syllabus was successfully deleted.'
   end
 
   private
-    def set_syllabus
-      @syllabus = Syllabus.find(params[:id])
-    end
+  def set_syllabus
+    @syllabus = Syllabus.find(params[:id])
+  end
 
-    def syllabus_params
-      params.require(:syllabus).permit(:title, :created_at, :file)
-    end
+  def syllabus_params
+    params.require(:syllabus).permit(:title, :created_at, :file)
+  end
+  
 end
