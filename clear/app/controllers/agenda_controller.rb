2ecf7ec8b7a42ac547207f6436737600cf2fad34
eulo0 <bobbyjones10102@gmail.com>
2026-01-31 08:00:57 +0000

    agenda items now use standard time instead of military time

--- clear/app/controllers/agenda_controller.rb
+++ clear/app/controllers/agenda_controller.rb
@@ -9,13 +9,11 @@ class AgendaController < ApplicationController
   day = Date.today.strftime("%A")
   day_to_abbreviation = {"Monday"=> "M", "Tuesday" => "T", "Wednesday" => "W", "Thursday" => "R", "Friday" => "F"}
   @events = current_user.events.where(starts_at: Date.today.all_day)
-  #@events = current_user.events.order(starts_at: :asc)
   @courses = Course.where("meeting_days LIKE ?", ["%",day_to_abbreviation[day],"%"].join)
-  #@courses = Course.where("meeting_days LIKE ?", "%R%")
   
-  @agenda = (@events.map { |e| {item: e, type: "Event", time: e.starts_at.strftime("%H:%M")}} +
-             @courses.map { |c| { item: c, type: "Course", time: c.start_time.strftime("%H:%M")}})
-            .sort_by {|entry| entry[:time]}
+  @agenda = (@events.map { |e| {item: e, type: "Event", time: e.starts_at.strftime("%I:%M %p")}} +
+           @courses.map { |c| { item: c, type: "Course", time: c.start_time.strftime("%I:%M %p")}})
+          .sort_by {|entry| entry[:time]}
   end
 
   def show; end
