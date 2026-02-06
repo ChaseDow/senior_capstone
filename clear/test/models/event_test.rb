a235f157062d8370d09bcfe2ab42bf7e25b3bebe
ChaseDow <chasedowdy46@gmail.com>
2026-02-06 17:33:19 -0600

    Setup fixtures and adjust tests

--- clear/test/models/event_test.rb
+++ clear/test/models/event_test.rb
@@ -1,44 +1,44 @@
 require "test_helper"
 
 class EventTest < ActiveSupport::TestCase
-  test "is valid with title and starts_at" do
-    event = Event.new(
-      title: "Study group",
-      starts_at: Time.current
-    )
+  fixtures :users
 
+  setup do
+    @user = users(:one)
+    @starts_at = Time.zone.local(2026, 1, 1, 10, 0, 0)
+  end
+
+  test "is valid with title and starts_at" do
+    event = build_event(title: "Study group", starts_at: @starts_at)
     assert event.valid?
   end
 
   test "requires a title" do
-    event = Event.new(starts_at: Time.current)
-
+    event = build_event(title: nil, starts_at: @starts_at)
     assert_not event.valid?
     assert event.errors[:title].any?
   end
 
   test "requires starts_at" do
-    event = Event.new(title: "No start time")
-
+    event = build_event(title: "No start time", starts_at: nil)
     assert_not event.valid?
     assert event.errors[:starts_at].any?
   end
 
   test "allows ends_at to be blank" do
-    event = Event.new(title: "No end time", starts_at: Time.current, ends_at: nil)
-
+    event = build_event(title: "No end time", starts_at: @starts_at, ends_at: nil)
     assert event.valid?
   end
 
   test "ends_at must be after starts_at when present" do
-    starts_at = Time.current
-    event = Event.new(
-      title: "Bad times",
-      starts_at: starts_at,
-      ends_at: starts_at - 1.hour
-    )
-
+    event = build_event(title: "Bad times", starts_at: @starts_at, ends_at: @starts_at - 1.hour)
     assert_not event.valid?
     assert event.errors[:ends_at].any?
+  end
+
+  private
+
+  def build_event(attrs = {})
+    Event.new({ user: @user, title: "Default", starts_at: @starts_at }.merge(attrs))
   end
 end
