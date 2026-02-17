4a52847e1bae7ec18c7f00e79c3f2d0f9d8ad81d
Chase <chase@Chase.localdomain>
2026-02-17 03:35:24 -0600

    Fix schedule to be user scoped for production

--- clear/app/controllers/schedule_controller.rb
+++ clear/app/controllers/schedule_controller.rb
@@ -1,18 +1,21 @@
-# frozen_string_literal:true
+# frozen_string_literal: true
 
 class ScheduleController < ApplicationController
-    layout "app_shell"
-    def week
-        reference_date = params[:date]&.to_date || Date.current
-        @week_start = reference_date.beginning_of_week(:monday)
-        @days = (0..6).map { |i| @week_start + i.days }
+  layout "app_shell"
+  before_action :authenticate_user!
 
-        items = Event.where(starts_at: @week_start..(@week_start + 6.days).end_of_day)
-        @items_by_day = items.group_by { |item| item.starts_at.to_date }
-        @items_by_day.transform_values! do |item|
-            item.sort_by(&:starts_at)
-        end
-    end
+  def week
+    reference_date = params[:date]&.to_date || Date.current
+    @week_start = reference_date.beginning_of_week(:monday)
+    @days = (0..6).map { |i| @week_start + i.days }
 
-    def show; end
+    items = current_user.events.where(
+      starts_at: @week_start..(@week_start + 6.days).end_of_day
+    )
+
+    @items_by_day = items.group_by { |item| item.starts_at.to_date }
+    @items_by_day.transform_values! { |day_items| day_items.sort_by(&:starts_at) }
+  end
+
+  def show; end
 end
