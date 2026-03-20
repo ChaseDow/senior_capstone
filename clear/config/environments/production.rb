a2371f132bb29c08eb4963928881b2b2cc95dad3
ChaseDow <chasedowdy46@gmail.com>
2026-03-20 16:02:18 -0500

    fix parsing

--- clear/config/environments/production.rb
+++ clear/config/environments/production.rb
@@ -21,8 +21,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # Store uploaded files in Digital Ocean Spaces (S3-compatible).
+  config.active_storage.service = :digital_ocean
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # config.assume_ssl = true
@@ -51,7 +51,6 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
