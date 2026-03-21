78b65c1d54cd5b92d94a612e24cc8d8eb287ee95
Chase <chase@Chase.localdomain>
2026-03-21 06:41:01 -0500

    modern left nav, new themes, and notice is a turboframe

---
+++ clear/app/javascript/controllers/toast_controller.js
@@ -0,0 +1,45 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = { duration: { type: Number, default: 4000 } }
+
+  connect() {
+    // Slide in
+    requestAnimationFrame(() => {
+      this.element.classList.remove("translate-x-full", "opacity-0")
+      this.element.classList.add("translate-x-0", "opacity-100")
+    })
+
+    // Start progress bar shrink
+    const bar = this.element.querySelector("[data-toast-progress]")
+    if (bar) {
+      requestAnimationFrame(() => {
+        bar.style.transition = `width ${this.durationValue}ms linear`
+        bar.style.width = "0%"
+      })
+    }
+
+    // Auto-dismiss
+    this.timer = setTimeout(() => this.dismiss(), this.durationValue)
+  }
+
+  disconnect() {
+    if (this.timer) clearTimeout(this.timer)
+  }
+
+  dismiss() {
+    if (this.timer) clearTimeout(this.timer)
+
+    this.element.classList.remove("translate-x-0", "opacity-100")
+    this.element.classList.add("translate-x-full", "opacity-0")
+
+    this.element.addEventListener("transitionend", () => {
+      this.element.remove()
+    }, { once: true })
+
+    // Fallback removal if transitionend doesn't fire
+    setTimeout(() => {
+      if (this.element.parentNode) this.element.remove()
+    }, 400)
+  }
+}
