43c8bff2ae429e9c2d896ab0c882524e73b767f9
eulo0 <bobbyjones10102@gmail.com>
2026-03-23 22:07:13 -0500

    For test and linting

--- clear/db/migrate/20260322035620_add_start_date_to_work_shifts.rb
+++ clear/db/migrate/20260322035620_add_start_date_to_work_shifts.rb
@@ -1,5 +1,5 @@
 class AddStartDateToWorkShifts < ActiveRecord::Migration[8.1]
   def change
-    add_column :work_shifts, :start_date, :date 
+    add_column :work_shifts, :start_date, :date
   end
 end
